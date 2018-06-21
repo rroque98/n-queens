b65a18910adc03e6138f9856867d2304690e6261
rroque98 <rroque98@gmail.com>
2018-06-21 13:15:26 -0700

    Finds a solution to rooks problem

--- src/solvers.js
+++ src/solvers.js
@@ -17,22 +17,62 @@
 // 
 
 window.findNRooksSolution = function(n) {
-  var solution = {}; //fixme
-  var board = new Board({'n': n});
-  var rook = 1;
-  // var rooksBoard = this.rows();
-  board.togglePiece(n - n, n - n);
-  solution[n - n] = n - n;
-  delete board.attributes[n - n]; // deleted row
-  
-  // put a piece in the first row and column
-  // keep track of position --> store in solution
-  // delete the row where the piece was placed --> do this by deleting the property from solution
 
+// togglePiece: function(rowIndex, colIndex)
+
+  // create a var to store solution
+  var solution;
+  // create our board with size defined by n
+  var board = new Board( { 'n' : n } );
   
+
+function test() {
+  for (var rowIndex = 0; rowIndex < board.rows().length; rowIndex++) {
+    for (var colIndex = 0; colIndex < board.rows()[rowIndex].length; colIndex++) {
+        board.togglePiece([rowIndex], [colIndex])
+      if(board.hasRowConflictAt([rowIndex]) || board.hasColConflictAt([colIndex])) {
+        board.togglePiece([rowIndex], [colIndex]);
+      } 
+
+    }
+  }
+}
+
+test();
+  
+  // Track: colIndexOfCurrentPiece, rowOfCurrentPiece, 
+  var recuse = function (colIndexOfCurrentPiece, rowOfCurrentPiece) {
+
+  // Start iterating at the last place of the first row
+    // check if that rook has a conflict with col or row
+    // if true
+      // start looping from the beginning of the board.
+        // check if first spot (index 0) is taken
+        // if it is, 
+          // continue to the next spot.
+        // else 
+          //toggle current piece from prev index
+          // toggle current index
+            //if there are no row or col conflicts at new position
+              // leave that piece in current place
+            // if there are
+              // move piece to next space available (recurse the process) 
+
+  //Take the next rook on the loop (decending)
+   //Start the same process applied to previous rook
+    //Repeat the process until loops ends (first element on the array)
+  };
+ 
+  // return the solution
+  solution = board.rows();
+
+  if(board.hasAnyRowConflicts()) {
+    return undefined;
+  } else {
+    return solution;
+  }
   
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
